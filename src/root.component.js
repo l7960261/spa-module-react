21ad299bde561e4f6297b6fc117f5f2cfc6f0abe
wilson <l7960261@gmail.com>
2019-05-30 16:48:40 +0800

    Add @reach/router

--- src/root.component.js
+++ src/root.component.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react"
+import { Link } from "@reach/router"
 
 export default class Root extends React.Component {
 
   render() {
     return (<div>
-      <h1>Root</h1>
-      <p><a href="/app1">app1</a></p>
-      <p><a href="/app2">app2</a></p>
-    </div>);
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/app1">App1</Link>
+      </nav>
+    </div>)
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    libraryTarget: 'amd',
+    // libraryTarget: 'amd',
     filename: '[name].bundle.js',
   },
   module: {
