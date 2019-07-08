28f9707e40b52c23cdea90b45dfb53149db4d7c3
wilson <l7960261@gmail.com>
2019-07-08 14:39:33 +0800

    Modify 'angular7' > 'angular'

--- src/root.component.js
+++ src/root.component.js
@@ -11,7 +11,7 @@ export default class Root extends React.Component {
         <Link to="/">Home</Link> |{" "}
         <Link to="/ng1">AngularJs</Link> |{" "}
         <Link to="/ng1-mixin">AngularJs with legacy module</Link> |{" "}
-        <Link to="/angular7">Angular7</Link>
+        <Link to="/angular">Angular</Link>
       </nav>
     </div>)
   }
